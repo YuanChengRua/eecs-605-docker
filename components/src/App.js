47a5470dd809b49c84b799239afc34560f2bad85
Yuan Cheng <79687050+YuanChengRua@users.noreply.github.com>
2022-03-25 15:57:55 -0400

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -66,7 +66,7 @@ function App() {
 
     // make POST request
     console.log('making POST request...');
-    fetch('<api-url>', {
+    fetch('https://8rozl92ff3.execute-api.us-east-1.amazonaws.com/prod/', {
       method: 'POST',
       headers: { "Content-Type": "application/json", "Accept": "text/plain" },
       body: JSON.stringify({ "csv": inputFileData })
