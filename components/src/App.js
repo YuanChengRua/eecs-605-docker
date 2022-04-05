bf2936d431f46e933220798f9116b7648d5d8490
Yuan Cheng <79687050+YuanChengRua@users.noreply.github.com>
2022-04-05 16:17:59 -0400

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -146,8 +146,8 @@ function App() {
         // POST request success
         else {
           const dropdownFileBytesData = JSON.parse(data.body)['bytesData'];
-          console.log(dropdownFileBytesData);
-          setInputFileData(decodeFileBase64(dropdownFileBytesData);
+          console.log(decodeFileBase64(dropdownFileBytesData));
+          setInputFileData(decodeFileBase64(dropdownFileBytesData));
           setInputImage('data:image/png;base64,' + dropdownFileBytesData); // hacky way of setting image from bytes data - even works on .jpeg lol
           setSubmitButtonText('Submit');
           setButtonDisable(false);
